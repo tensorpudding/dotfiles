3236bc1ed874ef71932ce61162422ddf4da11b1c
Michael <michael.l.moorman@gmail.com>
2010-06-30 13:56:06 -0400

    A simple xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,19 @@
---| XMonad configuration file
+-- 
+-- | XMonad configuration file
+--
 
 import XMonad
 import qualified XMonad.StackSet as W
 
 main :: IO ()
-main = undefined
+main = xmonad myconfig
+
+myconfig = defaultConfig {
+           workspaces = ["code","web","scratch"]
+         , terminal = "urxvtc"
+         , modMask = mod4Mask
+         , manageHook = composeAll
+                        [ className =? "Chromium" --> doF (W.shift "web")
+                        , className =? "Emacs"    --> doF (W.shift "code")
+                        ]
+         }
