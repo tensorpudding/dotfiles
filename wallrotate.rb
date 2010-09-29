4f1f04d5de9b8acce0a0d2ae24ec1ccd8193b4ea
Michael <michael.l.moorman@gmail.com>
2010-09-29 06:40:11 -0400

    updating wallrotate for gnome

--- wallrotate.rb
+++ wallrotate.rb
@@ -5,13 +5,13 @@
 #
 # Defaulting parameters which can be specified at the CLI
 #
-walldir = ARGV[0] || "/home/michael/wall/default/" 
+walldir = ARGV[0] || "/home/michael/Wallpapers/"
 #
 # Selects proper wallpaper randomly from the images in the directory
 #
 srand
-wallpaper = Dir[walldir + "*.{jpg,png,jpeg}"].choice
+wallpaper = Dir[walldir + "*.{jpg,png,jpeg,gif,BMP,bmp}"].choice
 #
 # Now it actually sets the wallpaper
 #
-%x{ /usr/bin/feh --bg-scale "#{ wallpaper }" } unless wallpaper.empty?
+%x{ gconftool-2 --type string --set /desktop/gnome/background/picture_filename "#{ wallpaper }" } unless wallpaper.empty?
